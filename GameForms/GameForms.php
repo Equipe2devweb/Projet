51b8123497c6bd27179db33c306e998af59f35b6
Equipe2devweb <138901361+Equipe2devweb@users.noreply.github.com>
2023-07-10 15:56:41 -0400

    Add files via upload
    2e version sans le Javascript. je suis blocker et chatGPT comprends pas non plus.

--- GameForms/GameForms.php
+++ GameForms/GameForms.php
@@ -1,56 +1,48 @@
-<?php
-// Retrieve the user's input from the form
-$userInput = $_POST['input'];
-$levelIndex = $_POST['levelIndex'];
+<!DOCTYPE html>
+<html>
 
-// Define the levels array with their expected answers
-$levels = [
-    [
-        'title' => "Level 1 - Arrange Letters in Ascending Order",
-        'expectedAnswer' => 'ABCDEF'
-    ],
-    [
-        'title' => "Level 2 - Arrange Letters in Descending Order",
-        'expectedAnswer' => 'FEDCBA'
-    ],
-    [
-        'title' => "Level 3 - Arrange Numbers in Ascending Order",
-        'expectedAnswer' => '123456'
-    ],
-    [
-        'title' => "Level 4 - Arrange Numbers in Descending Order",
-        'expectedAnswer' => '654321'
-    ],
-    [
-        'title' => "Level 5 - Identify the First and Last Letter",
-        'expectedAnswer' => 'AF'
-    ],
-    [
-        'title' => "Level 6 - Identify the Smallest and Largest Number",
-        'expectedAnswer' => '16'
-    ]
-];
+<head>
+    <title>Game Forms</title>
+    <link rel="stylesheet" type="text/css" href="GameForms.css">
+</head>
 
-// Check if the user input matches the expected answer for the current level
-if ($userInput === $levels[$levelIndex]['expectedAnswer']) {
-    // User succeeded in the current level
-    if ($levelIndex === count($levels) - 1) {
-        // User completed all levels
-        echo "<h1>Congratulations! You completed all levels successfully.</h1>";
-        echo "<p>Well done!</p>";
-        echo "<button onclick=\"location.href='GameForms.html'\">Play Again</button>";
-    } else {
-        // User completed the current level
-        echo "<h1>Success!</h1>";
-        echo "<p>Congratulations! You completed the level successfully.</p>";
-        echo "<p>Proceed to the next level.</p>";
-        echo "<button onclick=\"location.href='GameForms.html'\">Next Level</button>";
-    }
-} else {
-    // User failed in the current level
-    echo "<h1>Failure!</h1>";
-    echo "<p>Oops! Your arrangement is incorrect.</p>";
-    echo "<p>Please try again.</p>";
-    echo "<button onclick=\"location.href='GameForms.html'\">Try Again</button>";
-}
-?>
+<body>
+    <?php
+    $levels = include 'levels.php';
+
+    $levelIndex = $_POST['levelIndex'] ?? 0;
+    $level = $levels[$levelIndex];
+    ?>
+
+    <form id="gameForm" class="level" action="Game.php" method="POST">
+        <h1 id="levelTitle"><?php echo $level['title']; ?></h1>
+        <p id="levelDescription">Instructions: Arrange the given characters/numbers according to the specified order.</p>
+        <p id="lettersToArrange">
+    <?php
+    $expectedAnswer = $level['expectedAnswer'];
+    echo $expectedAnswer;
+    ?>
+</p>
+</p>
+        <?php
+        $numInputBoxes = $level['numInputBoxes'];
+        for ($i = 0; $i < $numInputBoxes; $i++) {
+            echo '<input type="text" name="input[]" required>';
+        }
+        ?>
+        <input type="hidden" id="levelIndex" name="levelIndex" value="<?php echo $levelIndex; ?>">
+        <button type="submit" name="submit">Submit</button>
+        <button id="giveUpButton" type="button">I Give up!</button>
+    </form>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
---
+++ GameForms/levels.php
@@ -0,0 +1,56 @@
+<?php
+return [
+    [
+        'title' => "Level 1 - Arrange Letters in Ascending Order",
+        'expectedAnswer' => generateRandomString(6),
+        'numInputBoxes' => 6
+    ],
+    [
+        'title' => "Level 2 - Arrange Letters in Descending Order",
+        'expectedAnswer' => generateRandomString(6),
+        'numInputBoxes' => 6
+    ],
+    [
+        'title' => "Level 3 - Arrange Numbers in Ascending Order",
+        'expectedAnswer' => generateRandomNumbers(6),
+        'numInputBoxes' => 6
+    ],
+    [
+        'title' => "Level 4 - Arrange Numbers in Descending Order",
+        'expectedAnswer' => generateRandomNumbers(6),
+        'numInputBoxes' => 6
+    ],
+    [
+        'title' => "Level 5 - Identify the First and Last Letter",
+        'expectedAnswer' => generateRandomString(2),
+        'numInputBoxes' => 2
+    ],
+    [
+        'title' => "Level 6 - Identify the Smallest and Largest Number",
+        'expectedAnswer' => generateRandomNumbers(2),
+        'numInputBoxes' => 2
+    ]
+];
+
+// Function to generate a random string of given length
+function generateRandomString($length)
+{
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $randomString;
+}
+
+
+// Function to generate an array of random numbers
+function generateRandomNumbers($length)
+{
+    $numbers = [];
+    for ($i = 0; $i < $length; $i++) {
+        $numbers[] = rand(1, 100);
+    }
+    return implode('', $numbers);
+}
+?>
