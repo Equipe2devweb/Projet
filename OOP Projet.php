be27865ef33c7bf21d544e49e66a6822741bedf2
Equipe2devweb <138901361+Equipe2devweb@users.noreply.github.com>
2023-07-11 10:07:07 -0400

    Add files via upload
    Fname Lname + selectDB

--- OOP Projet.php
+++ OOP Projet.php
@@ -2,8 +2,7 @@
 
 class DatabaseConnection
 {
-    private $connection;
-
+    private $connection;    
     public function __construct($hostname, $username, $password)
     {
         $this->connection = new mysqli($hostname, $username, $password);
@@ -35,6 +34,8 @@ class DatabaseConnection
         $sqlCreateTableQuery = "CREATE TABLE IF NOT EXISTS $tableName (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             username VARCHAR(30) NOT NULL,
+            firstname VARCHAR(255) NOT NULL,
+            lastname VARCHAR(255) NOT NULL,
             password VARCHAR(255) NOT NULL
         )";
 
@@ -42,12 +43,12 @@ class DatabaseConnection
             echo "Failed to create table: " . $this->connection->error;
         }
     }
-    public function insertData($tableName, $username, $password)
+    public function insertData($username, $firstname, $lastname, $password)
     {
         // Hash the password for security
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        $sqlInsertQuery = "INSERT INTO $tableName (username, password) VALUES ('$username', '$hashedPassword')";
+        $sqlInsertQuery = "INSERT INTO users (username, firstname, lastname, password) VALUES ('$username','$firstname','$lastname','$hashedPassword')";
 
         if ($this->connection->query($sqlInsertQuery) === FALSE) {
             echo "Failed to insert data: " . $this->connection->error;
@@ -208,19 +209,85 @@ public function insertScoreData($username, $score, $life, $flag1, $flag2, $flag3
         echo "Failed to insert data: " . $this->connection->error;
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+                                     //Utility//
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+ 
+function selectDatabase($database){
+    //Declare variables
+    global $connection;
+    //Attempt to connect to the database 
+    $select = mysqli_select_db($connection, $database);
+    if ($select === FALSE){
+        $errorMessage = mysqli_connect_error();
+        die("Selection of the database $database failed! <br>" . $errorMessage);
+    }
 }
+
+    public function displayUserDataWithScore()
+    {
+        $sqlQuery = "SELECT users.id, users.username, score.score, score.life, score.flag1, score.flag2, score.flag3, score.flag4, score.flag5, score.flag6
+                     FROM users
+                     INNER JOIN score ON users.id = score.otherTableId";
+    
+        $result = $this->connection->query($sqlQuery);
+    
+        if ($result === FALSE) {
+            echo "Failed to fetch data: " . $this->connection->error;
+        } else {
+            $data = [];
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+    
+            $this->displayTwoDimAssocArray($data);
+        }
+    }
+
+    private function displayTwoDimAssocArray($array)
+    {
+        echo "<table>";
+        echo "<tr><th>ID</th><th>Username</th><th>Score</th><th>Life</th><th>Flag1</th><th>Flag2</th><th>Flag3</th><th>Flag4</th><th>Flag5</th><th>Flag6</th></tr>";
+
+        foreach ($array as $item) {
+            echo "<tr>";
+            echo "<td>{$item['id']}</td>";
+            echo "<td>{$item['username']}</td>";
+            echo "<td>{$item['score']}</td>";
+            echo "<td>{$item['life']}</td>";
+            echo "<td>{$item['flag1']}</td>";
+            echo "<td>{$item['flag2']}</td>";
+            echo "<td>{$item['flag3']}</td>";
+            echo "<td>{$item['flag4']}</td>";
+            echo "<td>{$item['flag5']}</td>";
+            echo "<td>{$item['flag6']}</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    }
+}
+
+
+
 
 $hostname = 'localhost';
 $username = 'root';
 $password = 'simex911';
 
-$database = 'ProjetPHP';
+$database = 'ProjectPHP';
 
 // Create a new instance of the DatabaseConnection class
 $dbConnection = new DatabaseConnection($hostname, $username, $password);
 
 // Create the database if it doesn't exist
 $dbConnection->createDatabase($database);
+
+//insure we use this Database
+$dbConnection->selectDatabase($database);
 
 // Create Table 1 (users)
 $dbConnection->createTable1();
@@ -230,6 +297,8 @@ $dbConnection->createTable2();
 
 // Example usage
 $username = 'Runbooster';
+$firstname = "Simon";
+$lastname = 'Guay Gozzi';
 $score = 100;
 $life = 3;
 $flag1 = true;
@@ -241,5 +310,5 @@ $flag6 = false;
 
 $dbConnection->insertScoreData($username, $score, $life, $flag1, $flag2, $flag3, $flag4, $flag5, $flag6);
 
-
+$dbConnection->displayUserDataWithScore();
 ?>
